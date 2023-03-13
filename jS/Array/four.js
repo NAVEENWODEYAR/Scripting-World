189d0fadc560bacfa654ec28b208a2f0809eab28
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2023-03-13 19:44:54 +0530

    re

---
+++ jS/Array/four.js
@@ -0,0 +1,30 @@
+// Javascript to chek weather a given array is empty or not empty,.
+let a = [1,2,3,4,5];   // Create a new array.
+
+console.log(a); //  Print the array.
+
+console.log(a.length); //  Print the length of the array.
+
+    if (a.length > 0) {
+            console.log("The given array is not empty");
+    }
+    else {
+        console.log("The given array is empty");
+    }
+
+
+
+
+
+let b = [];     // Create a new array.
+
+console.log(b); //  Print the array.
+
+console.log(b.length); //  Print the length of the array.
+
+if (b.length > 0){
+    console.log("The given array is not empty.");
+}
+else {
+    console.log("The given arry is empty.");
+}
