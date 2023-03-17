ab2423672a6878db29b358169349f1c910722ec3
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2023-03-17 18:45:56 +0530

    a

---
+++ jS/Functions/date.js
@@ -0,0 +1,21 @@
+// Javascript functions to display the date and time., 
+
+let date = new Date();
+
+console.log(date);
+
+console.log(date.getFullYear());   // getFullYear() returns the year,.
+console.log(date.getMonth()+1);     // getMonth()+1 writtens the present month.,
+
+console.log(date.getMinutes());     // getMinutes() returns the minutes.,
+
+console.log(date.getHours());       // getHours() returns the hours.,
+
+console.log(date.getSeconds());       // getSeconds() returns the seconds.,
+
+console.log(date.getMilliseconds()); // getMiliseconds() returns the miliseconds.,
+
+console.log(date.getDay());             // getDay() returns the day of the week.,
+
+console.log(date.getTime());            // getTime() returns the number of milliseconds since January 1, 1970.,
+
