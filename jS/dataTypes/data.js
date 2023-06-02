2c438a43d8ffe1a3577ccb2aac75867c2beca122
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-02 18:51:35 +0530

    name

---
+++ jS/dataTypes/data.js
@@ -0,0 +1,37 @@
+// Data types in JS.,
+
+// Integer type(number)
+let a = 12;
+let c = 1.2;
+console.log(a); 
+console.log(typeof(a));
+
+console.log(c);
+console.log(typeof(c));
+
+
+// String
+var b = 'JavaScript';
+console.log(b);
+console.log(typeof(b));
+
+var b = 'J';
+console.log(b);
+console.log(typeof(b));
+
+
+// Boolean
+let bl = true;
+console.log(bl);
+console.log(typeof(bl));
+
+// Javascript Array 
+let ar = [1,3,5,7,9];
+console.log(ar);
+console.log(typeof (ar));
+
+// Javascript Object
+let ob = {id:1, name:'Ana'};
+console.log(ob);
+console.log(typeof (ob));
+
